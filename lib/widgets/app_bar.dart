3daef9d243b15eed1315e3e25ff8ee55e3e58012
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-10-27 16:55:56 +0300

    :bug: fixes on statefull widget

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  final String title;
+
+  const MyAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {
+          Navigator.popAndPushNamed(context, "/");
+        },
+        icon: const Icon(Icons.arrow_back),
+      ),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 32,
+        ),
+      ),
+    );
+  }
+}
